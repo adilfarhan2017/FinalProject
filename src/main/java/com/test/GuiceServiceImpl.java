847daf0b9c2cdb1856915ad1fa7c32cc2d88229a
adil <adil.farhan@gmail.com>
2018-05-13 17:24:57 +0530

    Change print message

--- src/main/java/com/test/GuiceServiceImpl.java
+++ src/main/java/com/test/GuiceServiceImpl.java
@@ -4,7 +4,7 @@ public class GuiceServiceImpl implements GuiceService{
 
 
     public String serviceMethod(String msg) {
-        return "Something " + msg;
+        return "Is it working " + msg;
     }
 }
 
