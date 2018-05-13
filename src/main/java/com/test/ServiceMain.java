27fa4344ff008a5590281a95501311acc53d4fac
adil.farhan@gmail.com <adil.farhan@gmail.com>
2018-05-13 19:12:44 +0530

    Comment added

--- src/main/java/com/test/ServiceMain.java
+++ src/main/java/com/test/ServiceMain.java
@@ -19,12 +19,14 @@ public class ServiceMain {
         injector.injectMembers(smain);
 
 
+
+
        for (int i=0;i<10;i++){
 
            System.out.println("Print "+i);
        }
           System.out.println(smain.gs.serviceMethod("Hello"));
-        System.out.println(smain.gs.serviceMethod("Hsaelloqasadwqwqwqwqwqwqw"));
+        System.out.println(smain.gs.serviceMethod("Hsaelloq"));
 
 
     }
